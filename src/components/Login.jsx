242ff95d647ba1107004a070c66f8b972c2b2d9d
BLACK <blackkitten010@gmail.com>
2022-10-21 16:31:43 +0700

    style login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {View, Text, TextInput, Image, Pressable, Alert, Button} from 'react-native';
+import {View, Text, TextInput, Image, Alert, TouchableOpacity} from 'react-native';
 
 import {Formik} from 'formik';
 import * as yup from 'yup'
 import logo from '../assets/images/logo.png';
 const Login = () => {
-  // const [text, onChangeText] = React.useState('Useless Text');
-  const [test, setTest] = React.useState('SIGN IN');
 
   return (
     <Formik
@@ -37,17 +35,18 @@ const Login = () => {
       }) => (
         <View className="min-h-full bg-base">
           <View className="bg-green-sod rounded-b-2xl h-[85%] shadow shadow-black">
-            <View className="items-center">
+            <View className="h-1/2"><View className="items-center">
               <Image source={logo} className="w-28 h-28" />
-            </View>
-
-            <View className="mx-3 justify-center">
+            </View></View>
+            
+          <View className=" h-1/2 justify-center">
+            <View className="mx-7 justify-center">
               <Text className="font-notobold text-white mb-2">Phone</Text>
               <TextInput
                 onChangeText={handleChange('phone')}
                 onBlur={() => setFieldTouched('phone')}
                 value={values.phone}
-                className=" bg-white rounded h-8 px-1 py-0"
+                className=" bg-white rounded h-8 px-1 py-0 font-noto"
               />
               {touched.phone && errors.phone && (
                 <Text style={{fontSize: 12, color: '#FF0D10'}}>
@@ -55,12 +54,12 @@ const Login = () => {
                 </Text>
               )}
 
-              <Text className="font-notobold text-white mb-2 mt-3">Email</Text>
+              <Text className="font-notobold text-white mb-2 mt-2">Email</Text>
               <TextInput
                 onChangeText={handleChange('email')}
                 onBlur={() => setFieldTouched('email')}
                 value={values.email}
-                className=" bg-white rounded h-8 px-1 py-0"
+                className=" bg-white rounded h-8 px-1 py-0 font-noto"
               />
               {touched.email && errors.email && (
                 <Text style={{fontSize: 12, color: '#FF0D10'}}>
@@ -68,14 +67,15 @@ const Login = () => {
                 </Text>
               )}
             </View>
+            </View>
           </View>
           <View className="h-[15%] mx-7">
-            <Pressable
+            <TouchableOpacity
               onPressOut={handleSubmit}
               disabled={!isValid}
               className="bg-green-oon rounded-lg items-center justify-center h-14 my-auto shadow shadow-black">
-              <Text className="font-noto text-xl text-white py-2">{test}</Text>
-            </Pressable>
+              <Text className="font-noto text-xl text-white py-2">SIGN IN</Text>
+            </TouchableOpacity>
           </View>
         </View>
       )}
